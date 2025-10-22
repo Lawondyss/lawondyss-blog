ebe9dbfa2363d44948fefd5d7283c26051f98b1a
Ladislav Vondráček <lawondyss@gmail.com>
2025-10-22 18:45:30 +0200

    Added view transition for navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type {Snippet} from 'svelte'
+  import {onNavigate} from '$app/navigation'
   import '$css/app.css'
   import '$css/code-highlight.css'
   import Page from '$composites/Page.svelte'
@@ -9,6 +10,18 @@
   }
 
   let {children}: Props = $props()
+
+
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return;
+
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve()
+        await navigation.complete
+      })
+    })
+  })
 </script>
 
 <Page>
